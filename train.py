15bc7885cb1d7dbe35e83ed93386fc37fbba1d1a
yardengitta <46743539+yardengitta@users.noreply.github.com>
2020-01-02 23:37:42 +0200

    Update train.py

--- train.py
+++ train.py
@@ -96,7 +96,7 @@ model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'
 
 # FIT / TRAIN model
 
-NumEpochs = 28
+NumEpochs = 56
 BatchSize = 512
 
 #model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
