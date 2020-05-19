46bac03de8530c832a8eab6ab251b99e6d46b127
yardengitta <46743539+yardengitta@users.noreply.github.com>
2020-05-19 11:50:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
+import os
+if not os.path.exists('output'):
+    os.mkdir('output')
 
+for x in range(10001,10010):
+   name_file = os.path.join("output", "data"+str(x)+".txt")
+   f = open(name_file, 'w')
+   f.write(str(x) + "\n" + str(20000-x))
+   f.close()
