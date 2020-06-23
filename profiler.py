38a952ad60b508747013ee6318a7d45480fc6942
yardengitta <46743539+yardengitta@users.noreply.github.com>
2020-06-23 17:34:59 +0300

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -1,132 +1,118 @@
 
-from datetime import datetime
-from packaging import version
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+
+
+input_len = 1000
+tsteps = 2
+lahead = 1
+batch_size = 1
+epochs = 5
+
+print("*" * 33)
+if lahead >= tsteps:
+    print("STATELESS LSTM WILL ALSO CONVERGE")
+else:
+    print("STATELESS LSTM WILL NOT CONVERGE")
+print("*" * 33)
+
+np.random.seed(1986)
+
+print('Generating Data...')
+
+
+def gen_uniform_amp(amp=1, xn=10000):
+
+    data_input = np.random.uniform(-1 * amp, +1 * amp, xn)
+    data_input = pd.DataFrame(data_input)
+    return data_input
+
+
+to_drop = max(tsteps - 1, lahead - 1)
+data_input = gen_uniform_amp(amp=0.1, xn=input_len + to_drop)
+
+expected_output = data_input.rolling(window=tsteps, center=False).mean()
+
+if lahead > 1:
+    data_input = np.repeat(data_input.values, repeats=lahead, axis=1)
+    data_input = pd.DataFrame(data_input)
+    for i, c in enumerate(data_input.columns):
+        data_input[c] = data_input[c].shift(i)
+
+expected_output = expected_output[to_drop:]
+data_input = data_input[to_drop:]
+
+
+def create_model(stateful):
+    model = Sequential()
+    model.add(LSTM(20,
+              input_shape=(lahead, 1),
+              batch_size=batch_size,
+              stateful=stateful))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
+print('Creating Stateful Model...')
+model_stateful = create_model(stateful=True)
+
+
+def split_data(x, y, ratio=0.8):
+    to_train = int(input_len * ratio)
+    to_train -= to_train % batch_size
+    x_train = x[:to_train]
+    y_train = y[:to_train]
+    x_test = x[to_train:]
+    y_test = y[to_train:]
+
+    # tweak to match with batch_size
+    to_drop = x.shape[0] % batch_size
+    if to_drop > 0:
+        x_test = x_test[:-1 * to_drop]
+        y_test = y_test[:-1 * to_drop]
+
+    # some reshaping
+    reshape_3 = lambda x: x.values.reshape((x.shape[0], x.shape[1], 1))
+    x_train = reshape_3(x_train)
+    x_test = reshape_3(x_test)
+
+    reshape_2 = lambda x: x.values.reshape((x.shape[0], 1))
+    y_train = reshape_2(y_train)
+    y_test = reshape_2(y_test)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+(x_train, y_train), (x_test, y_test) = split_data(data_input, expected_output)
+print('x_train.shape: ', x_train.shape)
+print('y_train.shape: ', y_train.shape)
+print('x_test.shape: ', x_test.shape)
+print('y_test.shape: ', y_test.shape)
+
+print('Creating Stateless Model...')
+model_stateless = create_model(stateful=False)
 
 import os
-from operator import itemgetter    
-import numpy as np
-import warnings
-warnings.filterwarnings('ignore')
-
-
-import tensorflow as tf
-
-print("TensorFlow version: ", tf.__version__)
-
-device_name = tf.test.gpu_device_name()
-if not device_name:
-  raise SystemError('GPU device not found')
-print('Found GPU at: {}'.format(device_name))
-
-from keras import models, regularizers, layers, optimizers, losses, metrics
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.utils import np_utils, to_categorical
-
- 
-from keras.datasets import imdb
-
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(path="imdb.npz", num_words=10000)
-
-print("train_data ", train_data.shape)
-print("train_labels ", train_labels.shape)
-print("_"*100)
-print("test_data ", test_data.shape)
-print("test_labels ", test_labels.shape)
-print("_"*100)
-
-# See an actual review in words
-# Reverse from integers to words using the DICTIONARY (given by keras...need to do nothing to create it)
-
-word_index = imdb.get_word_index()
-
-reverse_word_index = dict(
-[(value, key) for (key, value) in word_index.items()])
-
-# VECTORIZE as one cannot feed integers into a NN
-# Encoding the integer sequences into a binary matrix - one hot encoder basically
-# From integers representing words, at various lengths - to a normalized one hot encoded tensor (matrix) of 10k columns
-
-def vectorize_sequences(sequences, dimension=10000):
-    results = np.zeros((len(sequences), dimension))
-    for i, sequence in enumerate(sequences):
-        results[i, sequence] = 1.
-    return results
-
-x_train = vectorize_sequences(train_data)
-x_test = vectorize_sequences(test_data)
-
-print("x_train ", x_train.shape)
-print("x_test ", x_test.shape)
-
-
-# VECTORIZE the labels too - NO INTEGERS only floats into a tensor...(rare exceptions)
-
-y_train = np.asarray(train_labels).astype('float32')
-y_test = np.asarray(test_labels).astype('float32')
-print("y_train ", y_train.shape)
-print("y_test ", y_test.shape)
-
-# Set a VALIDATION set
-
-x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
-y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
-
-print("x_val ", x_val.shape)
-print("partial_x_train ", partial_x_train.shape)
-print("y_val ", y_val.shape)
-print("partial_y_train ", partial_y_train.shape)
-
-# NN MODEL
-
-# Use of DROPOUT
-#model = models.Sequential()
-#model.add(layers.Dense(16, kernel_regularizer=regularizers.l1(0.001), activation='relu', input_shape=(10000,)))
-#model.add(layers.Dropout(0.5))
-#model.add(layers.Dense(16, kernel_regularizer=regularizers.l1(0.001),activation='relu'))
-#model.add(layers.Dropout(0.5))
-#model.add(layers.Dense(1, activation='sigmoid'))
-
-# Use of REGULARIZATION
-model = models.Sequential()
-model.add(layers.Dense(16, kernel_regularizer=regularizers.l1_l2(l1=0.001, l2=0.001),activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, kernel_regularizer=regularizers.l1_l2(l1=0.001, l2=0.001),activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
-
-# REGULARIZERS L1 L2
-#regularizers.l1(0.001)
-#regularizers.l2(0.001)
-regularizers.l1_l2(l1=0.001, l2=0.001)
-
-# OPTIMIZERS
-#model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy, metrics=[metrics.binary_accuracy])
-model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
-
-# FIT / TRAIN model
-
-NumEpochs = 12
-BatchSize = 512
-
-
-# Create a TensorBoard callback
-logs = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-
-tboard_callback = tf.keras.callbacks.TensorBoard(log_dir = logs,
-                                                 histogram_freq = 1,
-                                                 profile_batch = '500,520')
-
-
-#model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-
-history = model.fit(partial_x_train, partial_y_train, epochs=NumEpochs, batch_size=BatchSize, validation_data=(x_val, y_val),callbacks = [tboard_callback])
-
-results = model.evaluate(x_test, y_test)
-print("_"*100)
-print("Test Loss and Accuracy")
-print("results ", results)
-
-if not os.path.exists('output'):
-    os.mkdir('output')
-model.save('output/sentiment2.model.h5')
+import datetime
+ROOT_DIR = os.getcwd()
+log_dir = os.path.join('callback_tests')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+print(log_dir)
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+                                       
+print('Training')
+history = model_stateless.fit(x_train,
+                    y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test),
+                    shuffle=False,
+                    callbacks=[tensorboard_callback]
+                    )
